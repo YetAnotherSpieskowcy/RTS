08863c65b826b20b97fd858a75fba40e250c2f04
Bogna Lew <100587236+BognaLew@users.noreply.github.com>
2023-05-05 20:04:18 +0200

    Update ReadmeEditor.cs

--- src/RTS-game/Assets/TutorialInfo/Scripts/Editor/ReadmeEditor.cs
+++ src/RTS-game/Assets/TutorialInfo/Scripts/Editor/ReadmeEditor.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 public class ReadmeEditor : Editor
 {
     static string s_ShowedReadmeSessionStateName = "ReadmeEditor.showedReadme";
-    
+
     static string s_ReadmeSourceDirectory = "Assets/TutorialInfo";
 
     const float k_Space = 16f;
@@ -24,7 +24,7 @@ public class ReadmeEditor : Editor
     static void RemoveTutorial()
     {
         if (EditorUtility.DisplayDialog("Remove Readme Assets",
-            
+
             $"All contents under {s_ReadmeSourceDirectory} will be removed, are you sure you want to proceed?",
             "Proceed",
             "Cancel"))
