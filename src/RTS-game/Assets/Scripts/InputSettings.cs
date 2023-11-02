e1407bf5bf718bd38ef2f9da4926aead344da218
Zofia Sosińska <98112758+spohie001@users.noreply.github.com>
2023-11-02 21:52:27 +0100

    Update InputSettings.cs

--- src/RTS-game/Assets/Scripts/InputSettings.cs
+++ src/RTS-game/Assets/Scripts/InputSettings.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public static class InputSettings
 {
-   public static KeyCode UnitSelectionMenuItem4 = KeyCode.Alpha4;
+   public static KeyCode UnitSelectionMenu = KeyCode.Q;
+    public static KeyCode UnitSelectionMenuItem1 = KeyCode.Alpha1;
+    public static KeyCode UnitSelectionMenuItem2 = KeyCode.Alpha2;
+    public static KeyCode UnitSelectionMenuItem3 = KeyCode.Alpha3;
+    public static KeyCode UnitSelectionMenuItem4 = KeyCode.Alpha4;
     public static KeyCode UnitSelectionMenuItem5 = KeyCode.Alpha5;
     public static KeyCode UnitSelectionMenuCancel = KeyCode.Alpha0;
     public static KeyCode ShowEnemyUnitMarker = KeyCode.F;
