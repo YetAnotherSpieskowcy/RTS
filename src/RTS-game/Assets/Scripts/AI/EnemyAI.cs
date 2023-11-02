ea28a02aac91bbaf885bff7afebe4cf0ba347894
Bartosz Strzelecki <48790127+bstrzelecki@users.noreply.github.com>
2023-11-02 21:00:52 +0100

    Added animals (#38)

--- src/RTS-game/Assets/Scripts/AI/EnemyAI.cs
+++ src/RTS-game/Assets/Scripts/AI/EnemyAI.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private AIAnimation anim;
     [HideInInspector] public Transform target;
 
     private Transform centerLock;
@@ -48,7 +49,7 @@ public class EnemyAI : MonoBehaviour
 
     public void MoveTo(Vector3 position)
     {
-        agent.destination = position;
+        agent.SetDestination(position);
     }
 
     public void Target(Vector3 target)
@@ -74,11 +75,16 @@ public class EnemyAI : MonoBehaviour
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<AIAnimation>();
         StoppingDistance = Radius;
     }
 
     void Update()
     {
+        if (anim != null)
+        {
+            anim.Move(agent.desiredVelocity);
+        }
         if (target != null)
         {
             agent.SetDestination(target.position);
