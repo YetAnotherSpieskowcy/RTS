a32f004b259c630126afe54f22cfe3b9b43a190c
spohie001 <z.sosinska001@gmail.com>
2023-11-26 18:36:30 +0100

    added player hp

--- src/RTS-game/Assets/Scripts/Unit.cs
+++ src/RTS-game/Assets/Scripts/Unit.cs
@@ -34,6 +34,8 @@ public class Unit : MonoBehaviour
         return !isDead;
     }
     [SerializeField] private int health = 100;
+    public int Health { get => health; }
+
     public void Hit(int damage)
     {
         health -= damage;
