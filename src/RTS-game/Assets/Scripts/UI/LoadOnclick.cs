90dba027bf9fb3d39b1b0fef50d220424ce5c950
spohie001 <z.sosinska001@gmail.com>
2023-12-03 17:28:53 +0100

    loading works

--- src/RTS-game/Assets/Scripts/UI/LoadOnclick.cs
+++ src/RTS-game/Assets/Scripts/UI/LoadOnclick.cs
@@ -10,22 +10,10 @@ using System.Diagnostics;
 public class LoadOnclick : MonoBehaviour
 {
     public TMP_Text txt;
-    private bool canLoad = false;
+    private LoadFromAnotherScene load;
     public void LoadGame()
     {
+        LoadFromAnotherScene.NameOfSaveFile_S = txt.text;
         SceneManager.LoadScene(1);
-        canLoad = true;
-    }
-    void Update()
-    {
-        if (SceneManager.GetSceneByBuildIndex(1).IsValid() && canLoad)
-        {
-            string saveName = txt.text;
-            string basePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string dir = "SuwakGame";
-            saveName = txt.text; saveName = txt.text; saveName = txt.text; saveName = txt.text; saveName = txt.text;
-            SaveManager.Load(Path.Combine(basePath, dir, saveName).ToString());
-            canLoad = false;
-        }
     }
 }
