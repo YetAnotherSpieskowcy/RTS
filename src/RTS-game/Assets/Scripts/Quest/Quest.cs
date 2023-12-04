ff92ddb597f6a661ef93d7051a264afb75f4b578
spohie001 <z.sosinska001@gmail.com>
2023-12-04 14:57:19 +0100

    added quest journal

--- src/RTS-game/Assets/Scripts/Quest/Quest.cs
+++ src/RTS-game/Assets/Scripts/Quest/Quest.cs
@@ -1,12 +1,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using TMPro;
 
 public class Quest : MonoBehaviour
 {
+    public GameObject questJournalObject;
     public List<QuestObjective> objectives;
 
     private bool isStarted = false;
+
+    public GameObject GetQuestJournalObject()
+    {
+        return questJournalObject;
+    }
     public void StartQuest()
     {
         isStarted = true;
---
+++ src/RTS-game/Assets/Scripts/UI/QuestInfoController.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class QuestInfoController : MonoBehaviour
+{
+    public GameObject questJournal;
+    public GameObject questJournalContent;
+
+    private List<Quest> questsList;
+    private GameObject questActive;
+    private bool isVisible;
+
+    void Start()
+    {
+        questsList = ((Quest[])GameObject.FindObjectsOfType(typeof(Quest))).ToList();
+        questActive = GameObject.Find("QuestActive");
+        questActive.SetActive(false);
+        HideAtStart();
+        isVisible = true;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(InputSettings.Journal))
+        {
+            Debug.Log("Journal");
+            if (isVisible)
+            {
+                ShowQuestJournal();
+                isVisible = false;
+            }
+            else
+            {
+                HideQuestJournal();
+                isVisible = true;
+            }
+        }
+        UpdateQuestMarker();
+    }
+
+    private void ShowQuestJournal()
+    {
+        questJournal.SetActive(true);
+        Cursor.lockState = CursorLockMode.Confined;
+        foreach (var q in questsList)
+        {
+            GameObject qJournalObject = q.GetQuestJournalObject();
+            GameObject notCompleted = GetAlertObject(qJournalObject.transform);
+
+            if (q.IsStarted())
+            {
+                qJournalObject.SetActive(true);
+                qJournalObject.transform.SetParent(questJournalContent.transform);
+            }
+
+            if (!q.IsCompleted())
+            {
+                notCompleted.SetActive(true);
+            }
+            else
+            {
+                notCompleted.SetActive(false);
+            }
+        }
+
+    }
+
+    private void HideAtStart()
+    {
+        foreach (var q in questsList)
+        {
+            GameObject qJournalObject = q.GetQuestJournalObject();
+            GameObject notCompleted = GetAlertObject(qJournalObject.transform);
+
+            qJournalObject.transform.parent = null;
+            notCompleted.SetActive(false);
+            qJournalObject.SetActive(false);
+        }
+
+    }
+
+    private void HideQuestJournal()
+    {
+        foreach (var q in questsList)
+        {
+            GameObject qJournalObject = q.GetQuestJournalObject();
+            GameObject notCompleted = GetAlertObject(qJournalObject.transform);
+
+            qJournalObject.transform.parent = null;
+            notCompleted.SetActive(false);
+            qJournalObject.SetActive(false);
+        }
+        questJournal.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    private void UpdateQuestMarker()
+    {
+        bool anyIsStarted = false;
+        foreach (var q in questsList)
+        {
+            if (q.IsStarted())
+            {
+                anyIsStarted = true;
+            }
+        }
+
+        if (anyIsStarted)
+        {
+            bool allStartedAreCompleted = true;
+            foreach (var q in questsList)
+            {
+                if (q.IsStarted())
+                {
+                    if (!q.IsCompleted())
+                    {
+                        allStartedAreCompleted = false;
+                    }
+                }
+            }
+
+            if (!allStartedAreCompleted)
+            {
+                questActive.SetActive(true);
+            }
+            else
+            {
+                questActive.SetActive(false);
+            }
+        }
+        else
+        {
+            questActive.SetActive(false);
+        }
+    }
+
+    private GameObject GetAlertObject(Transform b)
+    {
+        foreach (Transform child in b)
+        {
+            if (child.tag == "Alert") return child.gameObject;
+        }
+        return null;
+    }
+}
