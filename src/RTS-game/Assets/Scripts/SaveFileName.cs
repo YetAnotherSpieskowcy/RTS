d3942b46115fa163e7c03321277f114ba1c8862f
spohie001 <z.sosinska001@gmail.com>
2023-12-05 10:49:48 +0100

    minor changes

---
+++ src/RTS-game/Assets/Scripts/SaveFileName.cs
@@ -0,0 +1,18 @@
+sing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveFileName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
